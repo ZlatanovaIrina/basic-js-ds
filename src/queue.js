9ea0eb949c7844337bcb217aee7a0a54b6c3a776
ZlatanovaIrina <T0ffy@ya.ru>
2022-10-17 17:59:52 +0300

    feat: remove-from-list done

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+ class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class Queue {
+  constructor() {
+    this.queue = new ListNode();
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.queue;
+  }
+
+  enqueue(value) {
+    return this.queue.value = value;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue.value;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  let current = l.next;
+  
+  while (current) {
+    if (current.value === k) {
+      l.next.next = current.next;
+    } 
+      current = current.next;
+  } 
+  return l;
+
 }
 
 module.exports = {
