cc338003dc6b45ea29c97adf1d9f98bef56b2d8a
Zlatanova Irina <t0ffy@ya.ru>
2025-01-05 23:32:26 +0200

    feat: implement Queue class in queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,28 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
- class ListNode {
-  constructor(x) {
-    this.value = x;
-    this.next = null;
-  }
-}
 
 class Queue {
-  constructor() {
-    this.queue = new ListNode();
-  }
+
+  head = null;
+  tail = this.head;
 
   getUnderlyingList() {
-    return this.queue;
+    return this.head;
   }
 
   enqueue(value) {
-    return this.queue.value = value;
+    if (!this.head) {
+      this.head = new ListNode(value);
+      this.tail = this.head;
+      return;
+    }
+    
+    const currentTail = this.tail;
+    this.tail = new ListNode(value);;
+    currentTail.next = this.tail;
   }
 
   dequeue() {
-    return this.queue.value;
+    if (!this.head) {
+      return;
+    }
+    const currentHeadValue = this.head.value;
+    this.head.value = this.head.next.value;
+    this.head.next = this.head.next.next;
+    return currentHeadValue;
   }
 }
 
